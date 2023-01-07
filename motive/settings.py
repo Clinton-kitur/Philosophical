2d03f228be40e96ef9c1537d94a04dd84b7d3de0
Clinton <110016135+ClintonPy@users.noreply.github.com>
2023-01-07 15:44:26 +0300

    Add files via upload
    setting.[y update

--- motive/settings.py
+++ motive/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7=b#$ubbisl^qoig6@_4+brkzxd47r6jzes%b2x05q4g11_*gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [127.0.0.1]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'philo-quotes.onrender.com']
 
 
 # Application definition
