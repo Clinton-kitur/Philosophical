cfe0948ea4f8b6fd0d2e4520856f902c3a848e38
Clinton <110016135+ClintonPy@users.noreply.github.com>
2023-01-07 19:59:22 +0300

    Add files via upload
    setting.py update

--- motive/settings.py
+++ motive/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7=b#$ubbisl^qoig6@_4+brkzxd47r6jzes%b2x05q4g11_*gk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'qute.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
