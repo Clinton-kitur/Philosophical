69d8da375b4b5b6d5ae3a53b8d129ca71d5211ef
Clinton <110016135+ClintonPy@users.noreply.github.com>
2023-01-09 10:02:28 +0300

    Update settings.py

--- motive/settings.py
+++ motive/settings.py
@@ -22,23 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv(BASE_DIR / '.env')
-
-
-# core/settings.py
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Quick-start development settings - unsuitable for production
