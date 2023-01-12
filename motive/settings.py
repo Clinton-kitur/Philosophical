20d7ac8ff4caf0910b0c8ae779a1a211f13cb1af
Clinton <110016135+ClintonPy@users.noreply.github.com>
2023-01-12 11:01:28 +0300

    Update settings.py

--- motive/settings.py
+++ motive/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-7=b#$ubbisl^qoig6@_4+brkzxd47r6jzes%b2x05q4g11_*gk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://philosophi.onrender.com"]
 
 
 # Application definition
