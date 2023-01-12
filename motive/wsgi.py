aa93cc6d43682e7eb76adfe717cf664b8650172c
Clinton <110016135+ClintonPy@users.noreply.github.com>
2023-01-12 10:51:43 +0300

    Add files via upload

--- motive/wsgi.py
+++ motive/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motive.settings')
 
-application = get_wsgi_application()
+gunicorn -w 4 motive.wsgi:application
