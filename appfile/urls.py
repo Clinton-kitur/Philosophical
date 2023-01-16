05537358bf75a835a9f45ae96aeb2aa2eefb06d0
Clinton <110016135+ClintonPy@users.noreply.github.com>
2023-01-16 11:46:28 +0300

    Update urls.py

--- appfile/urls.py
+++ appfile/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('quotes/<int:pk>/', views.QuoteDetailView.as_view(), name='quote_detail'),
+    path('create-quote/', QuoteCreateView.as_view(), name='create_quote'),
 ]
